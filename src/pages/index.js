8df34c317cf769f831aa0a7f6e6a33d7a3cc3fd3
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-05-10 22:27:57 -0500

    Add info component with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,12 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import { Button } from "../components/Button"
 import { FaApple } from "react-icons/fa"
+import Info from "../components/info"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Info />
     <h1 style={{ color: "red" }}>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
